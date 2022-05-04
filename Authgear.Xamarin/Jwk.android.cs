cd512dd8c48ece40f867a17483833ac32714a331
Roxk <vzox.rock@gmail.com>
2022-05-04 12:35:50 +0800

    Add Jwk #4

---
+++ Authgear.Xamarin/Jwk.android.cs
@@ -0,0 +1,24 @@
+﻿using Android.Runtime;
+using Authgear.Xamarin.CsExtensions;
+using Java.Security;
+using Java.Security.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authgear.Xamarin
+{
+    internal static class JwkExtensions
+    {
+        public static Jwk FromPublicKey(string kid, IPublicKey publicKey)
+        {
+            var rsaPublicKey = publicKey.JavaCast<IRSAPublicKey>();
+            return new Jwk
+            {
+                Kid = kid,
+                N = ConvertExtensions.ToBase64UrlSafeString(rsaPublicKey.Modulus.ToByteArray()),
+                E = ConvertExtensions.ToBase64UrlSafeString(rsaPublicKey.PublicExponent.ToByteArray())
+            };
+        }
+    }
+}
---
+++ Authgear.Xamarin/Jwk.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Authgear.Xamarin
+{
+    internal class Jwk
+    {
+        [JsonPropertyName("kid")]
+        public string Kid { get; set; }
+        [JsonPropertyName("alg")]
+        public string Alg { get; set; } = "RS256";
+        [JsonPropertyName("kty")]
+        public string Kty { get; set; } = "RSA";
+        [JsonPropertyName("n")]
+        public string N { get; set; }
+        [JsonPropertyName("e")]
+        public string E { get; set; }
+    }
+}
