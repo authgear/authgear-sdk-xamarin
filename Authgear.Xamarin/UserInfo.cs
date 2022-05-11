90a43a90057d9e34f7d04a669ce3a3a21dca7e01
Louis Chan <louischan@oursky.com>
2022-05-11 15:48:55 +0800

    Fill in missing property in UserInfo

--- Authgear.Xamarin/UserInfo.cs
+++ Authgear.Xamarin/UserInfo.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Authgear.Xamarin
 {
     public class UserInfo
     {
+        [JsonPropertyName("sub")]
+        public string Sub { get; set; }
+        [JsonPropertyName("https://authgear.com/claims/user/is_anonymous")]
+        public bool IsAnonymous { get; set; }
+        [JsonPropertyName("https://authgear.com/claims/user/is_verified")]
+        public bool IsVerified { get; set; }
     }
 }
