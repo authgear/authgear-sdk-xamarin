9e03f8ca3e371d38a2dc8b5efb39e035ec6eeee8
Roxk <vzox.rock@gmail.com>
2022-05-13 18:57:17 +0800

    Use native API to grab ios device info

--- Authgear.Xamarin/DeviceInfo/DeviceInfoIos.ios.cs
+++ Authgear.Xamarin/DeviceInfo/DeviceInfoIos.ios.cs
@@ -1,11 +1,53 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using Foundation;
+using ObjCRuntime;
 using UIKit;
+using Xamarin.Essentials;
 
 namespace Authgear.Xamarin
 {
     public partial class DeviceInfoIos
     {
+        [DllImport(Constants.SystemLibrary, EntryPoint = "sysctlbyname")]
+        internal static extern int SysctlByName([MarshalAs(UnmanagedType.LPStr)] string property, IntPtr output, IntPtr oldLen, IntPtr newp, uint newlen);
+
+        internal static string GetSystemLibraryProperty(string property)
+        {
+            var lengthPtr = Marshal.AllocHGlobal(sizeof(int));
+            SysctlByName(property, IntPtr.Zero, lengthPtr, IntPtr.Zero, 0);
+
+            var propertyLength = Marshal.ReadInt32(lengthPtr);
+
+            if (propertyLength == 0)
+            {
+                Marshal.FreeHGlobal(lengthPtr);
+                throw new InvalidOperationException("Unable to read length of property.");
+            }
+
+            var valuePtr = Marshal.AllocHGlobal(propertyLength);
+            SysctlByName(property, valuePtr, lengthPtr, IntPtr.Zero, 0);
+
+            var returnValue = Marshal.PtrToStringAnsi(valuePtr);
+
+            Marshal.FreeHGlobal(lengthPtr);
+            Marshal.FreeHGlobal(valuePtr);
+
+            return returnValue;
+        }
+
+        private static string GetBySysCtlName(string name)
+        {
+            try
+            {
+                return GetSystemLibraryProperty(name);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
         public static DeviceInfoIos Get()
         {
             var infoDict = NSBundle.MainBundle.InfoDictionary;
@@ -14,11 +56,11 @@ namespace Authgear.Xamarin
                 UName = new DeviceInfoIosUname
                 {
                     // These are best-effort approximation
-                    Machine = System.Environment.MachineName,
-                    NodeName = System.Environment.UserDomainName,
-                    Release = System.Environment.OSVersion.Version.ToString(),
-                    SysName = System.Environment.OSVersion.Platform.ToString(),
-                    Version = System.Environment.OSVersion.VersionString,
+                    Machine = GetBySysCtlName("hw.machine"),
+                    NodeName = DeviceInfo.Name,
+                    Release = GetBySysCtlName("kern.osrelease"),
+                    SysName = GetBySysCtlName("kern.ostype"),
+                    Version = GetBySysCtlName("kern.version"),
                 },
                 UiDevice = new DeviceInfoIosUiDevice
                 {
