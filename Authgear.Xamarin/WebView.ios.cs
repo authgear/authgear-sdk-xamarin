93e5010374c231de688a2443f51d5190fb29bb24
Roxk <vzox.rock@gmail.com>
2022-05-13 11:49:42 +0800

    Add openurl ios #6

--- Authgear.Xamarin/WebView.ios.cs
+++ Authgear.Xamarin/WebView.ios.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 
 namespace Authgear.Xamarin
 {
     internal class WebView : IWebView
     {
-        public Task ShowAsync(string url)
+        public async Task ShowAsync(string url)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _ = await WebAuthenticator.AuthenticateAsync(new WebAuthenticatorOptions
+                {
+                    Url = new Uri(url),
+                    CallbackUrl = new Uri("nocallback:///"),
+                    PrefersEphemeralWebBrowserSession = true
+                });
+            }
+            catch (OperationCanceledException)
+            {
+                // Cancel = going back, which is expected in this case, no-op.
+            }
         }
     }
 }
