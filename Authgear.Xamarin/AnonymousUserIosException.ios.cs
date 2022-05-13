4df684007e8bb0d99ba91165c5f640c1b9c230e2
Roxk <vzox.rock@gmail.com>
2022-05-13 11:40:52 +0800

    Add anonymous user ios #6

---
+++ Authgear.Xamarin/AnonymousUserIosException.ios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Foundation;
+using Security;
+
+namespace Authgear.Xamarin
+{
+    internal class AnonymousUserIosException : AuthgearException
+    {
+        public NSError Error { get; }
+
+        public AnonymousUserIosException(NSError error) : base("")
+        {
+            Error = error;
+        }
+
+        public AnonymousUserIosException(SecStatusCode code) : this(new NSError(NSError.OsStatusErrorDomain, Convert.ToInt32(code)))
+        {
+
+        }
+    }
+}
--- Authgear.Xamarin/Data/KeyRepo.ios.cs
+++ Authgear.Xamarin/Data/KeyRepo.ios.cs
@@ -2,19 +2,109 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Security;
+using UIKit;
 
 namespace Authgear.Xamarin.Data
 {
     internal class KeyRepo : IKeyRepo
     {
+        private const string TagFormat = "com.authgear.keys.biometric.{0}";
+        private const int KeySize = 2048;
+
         public Task<KeyJwtResult> GetOrCreateAnonymousJwtAsync(string keyId, string challenge, DeviceInfoRoot deviceInfo)
         {
-            throw new NotImplementedException();
+            EnsureApiLevel();
+            SecKey privateKey;
+            if (keyId == null)
+            {
+                keyId = Guid.NewGuid().ToString();
+                var tag = string.Format(TagFormat, keyId);
+                privateKey = GeneratePrivateKey(tag);
+            }
+            else
+            {
+                var tag = string.Format(TagFormat, keyId);
+                privateKey = GetPrivateKey(tag);
+                if (privateKey == null)
+                {
+                    throw new AnonymousUserNotFoundException();
+                }
+            }
+            var jwk = Jwk.FromPrivateKey(keyId, privateKey);
+            var header = new JwtHeader
+            {
+                Typ = JwtHeaderType.Anonymous,
+                Jwk = jwk,
+                Alg = jwk.Alg,
+                Kid = jwk.Kid,
+            };
+            var payload = new JwtPayload(DateTimeOffset.Now, challenge, "auth", deviceInfo);
+            var jwt = Jwt.Sign(privateKey, header, payload);
+            return Task.FromResult(new KeyJwtResult
+            {
+                KeyId = keyId,
+                Jwt = jwt
+            });
         }
 
         public Task<string> PromoteAnonymousUserAsync(string keyId, string challenge, DeviceInfoRoot deviceInfo)
         {
             throw new NotImplementedException();
+        }
+
+        private void EnsureApiLevel()
+        {
+            if(!UIDevice.CurrentDevice.CheckSystemVersion(11, 3))
+            {
+                throw new InvalidOperationException("Anonymous user is only supported on 11.3+");
+            }
+        }
+
+        private SecKey GeneratePrivateKey(string tag)
+        {
+            var keyGenParam = new SecKeyGenerationParameters()
+            {
+                KeyType = SecKeyType.RSA,
+                KeySizeInBits = KeySize
+            };
+            var privateKey = SecKey.CreateRandomKey(keyGenParam.Dictionary, out var error);
+            if (error != null)
+            {
+                throw AuthgearException.Wrap(new AnonymousUserIosException(error));
+            }
+            var secRecord = new SecRecord(privateKey)
+            {
+                ApplicationTag = tag
+            };
+            var status = SecKeyChain.Add(secRecord);
+            if (status != SecStatusCode.Success)
+            {
+                throw AuthgearException.Wrap(new AnonymousUserIosException(status));
+            }
+            return privateKey;
+        }
+
+        private SecKey GetPrivateKey(string tag)
+        {
+            var secRecord = new SecRecord(SecKind.Key)
+            {
+                KeyType = SecKeyType.RSA,
+                ApplicationTag = tag
+            };
+            var privateKey = SecKeyChain.QueryAsConcreteType(secRecord, out var result);
+            if (result != SecStatusCode.Success)
+            {
+                return null;
+            }
+            try
+            {
+                return (SecKey)privateKey;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
