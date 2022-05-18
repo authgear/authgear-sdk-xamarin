f0fd3f388c1bfd8762ad26ad373f9e1e8fd6b92d
Roxk <vzox.rock@gmail.com>
2022-05-18 16:31:35 +0800

    Add json converter tests #14

---
+++ UnitTest/JsonEnumConverterTests.cs
@@ -0,0 +1,33 @@
+﻿using Authgear.Xamarin.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTest
+{
+    [JsonConverter(typeof(JsonDescriptionEnumConverter<TestEnum>))]
+    internal enum TestEnum
+    {
+        [Description("test_enum_a")]
+        A,
+        [Description("test_enum_b")]
+        B
+    }
+
+    public class JsonEnumConverterTests
+    {
+        [Fact]
+        public void JsonEnum_Correct()
+        {
+            var value = TestEnum.A;
+            var json = JsonSerializer.Serialize(value);
+            Assert.Equal("\"test_enum_a\"", json);
+        }
+    }
+}
