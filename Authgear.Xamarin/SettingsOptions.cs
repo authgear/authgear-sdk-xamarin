6c6f47d1662f0b3940648278683da72646bcf977
Roxk <vzox.rock@gmail.com>
2022-05-23 12:45:59 +0800

    Add ui locales in settings options #37

--- Authgear.Xamarin/SettingsOptions.cs
+++ Authgear.Xamarin/SettingsOptions.cs
@@ -8,6 +8,7 @@ namespace Authgear.Xamarin
     public class SettingsOptions
     {
         public ColorScheme? ColorScheme { get; set; }
+        public List<string> UiLocales { get; set; }
         internal OidcAuthenticationRequest ToRequest(string url, string loginHint, bool suppressIdpSessionCookie)
         {
             return new OidcAuthenticationRequest
@@ -18,6 +19,7 @@ namespace Authgear.Xamarin
                 Prompt = new List<PromptOption>() { PromptOption.None },
                 LoginHint = loginHint,
                 ColorScheme = ColorScheme,
+                UiLocales = UiLocales,
                 SuppressIdpSessionCookie = suppressIdpSessionCookie,
             };
         }
