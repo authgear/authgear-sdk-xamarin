e0ca7dbd9d759f653c077e2ce15226335b2f25d9
Louis Chan <louischan@oursky.com>
2022-06-10 20:01:57 +0800

    Decode full UserInfo

---
+++ Authgear.Xamarin/UserInfoAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Authgear.Xamarin
+{
+    public class UserInfoAddress
+    {
+        [JsonPropertyName("formatted")]
+        public string? Formatted { get; set; }
+        [JsonPropertyName("street_address")]
+        public string? StreetAddress { get; set; }
+        [JsonPropertyName("locality")]
+        public string? Locality { get; set; }
+        [JsonPropertyName("region")]
+        public string? Region { get; set; }
+        [JsonPropertyName("postal_code")]
+        public string? PostalCode { get; set; }
+        [JsonPropertyName("country")]
+        public string? Country { get; set; }
+    }
+}
